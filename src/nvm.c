964c0dd97a46716e47792f530b76e7d6dd50aad1
ziga_miklosic <ziga.miklosic@gmail.com>
2021-06-26 09:44:12 +0200

    - fixing bug: region address offset was missing - cleaning code - updated version notes - updated template files

--- src/nvm.c
+++ src/nvm.c
@@ -38,12 +38,19 @@ static bool gb_is_init = false;
  */
 static const nvm_region_t * gp_nvm_regions = NULL;
 
-
 ////////////////////////////////////////////////////////////////////////////////
 // Functions
 ////////////////////////////////////////////////////////////////////////////////
 
-
+////////////////////////////////////////////////////////////////////////////////
+/**
+*		Initialized NVM regions
+*
+* @brief	Low level memory interface drivers are being initialized.
+*
+* @return 	status - Status of initialization
+*/
+////////////////////////////////////////////////////////////////////////////////
 nvm_status_t nvm_init(void)
 {
 	nvm_status_t 	status 	= eNVM_OK;
@@ -58,24 +65,46 @@ nvm_status_t nvm_init(void)
 	{
 		if ( NULL != gp_nvm_regions[reg_num].p_driver->pf_nvm_init )
 		{
-			gp_nvm_regions[reg_num].p_driver->pf_nvm_init();
+			status |= gp_nvm_regions[reg_num].p_driver->pf_nvm_init();
+
+			NVM_DBG_PRINT( "NVM: Region <%s> initialize with status: %d", gp_nvm_regions[reg_num].name, status );
 		}
 	}
 
+	NVM_ASSERT( eNVM_OK == status );
 
-
-	// TODO: Remove only testing...
+	// Init done
 	gb_is_init = true;
-
 
 	return status;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/**
+*		Get NVM initialization state
+*
+* @return 	gb_is_init - Initialization flag
+*/
+////////////////////////////////////////////////////////////////////////////////
 bool nvm_is_init(void)
 {
 	return gb_is_init;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/**
+*		Write data to NVM region
+*
+* @note		Input address argument of write function in NVM region is
+* 			offset by defined start address of region!
+*
+* @param[in]	region	- NVM region defined in config table
+* @param[in]	addr	- Start region address + address
+* @param[in]	size	- Size of written data in bytes
+* @param[in]	p_data	- Pointer to written data
+* @return 		status	- Status of operation
+*/
+////////////////////////////////////////////////////////////////////////////////
 nvm_status_t nvm_write(const nvm_region_name_t region, const uint32_t addr, const uint32_t size, const uint8_t * const p_data)
 {
 	nvm_status_t status = eNVM_OK;
@@ -85,14 +114,32 @@ nvm_status_t nvm_write(const nvm_region_name_t region, const uint32_t addr, cons
 
 	// Check inputs
 	NVM_ASSERT( region < eNVM_REGION_NUM_OF );
-	NVM_ASSERT(( addr >= gp_nvm_regions[region].start_addr ) && (( addr + size ) < ( gp_nvm_regions[region].start_addr + gp_nvm_regions[region].size )));
+	NVM_ASSERT(		(( addr + gp_nvm_regions[region].start_addr ) < ( gp_nvm_regions[region].start_addr + gp_nvm_regions[region].size ))
+				&& 	(( addr + gp_nvm_regions[region].start_addr + size ) < ( addr + gp_nvm_regions[region].start_addr + gp_nvm_regions[region].size )));
 
-	gp_nvm_regions[region].p_driver->pf_nvm_write( addr, size, p_data );
+	// Write
+	if ( eNVM_OK != gp_nvm_regions[region].p_driver->pf_nvm_write( gp_nvm_regions[region].start_addr + addr, size, p_data ))
+	{
+		status = eNVM_ERROR;
+	}
 
 	return status;
 }
 
-
+////////////////////////////////////////////////////////////////////////////////
+/**
+*		Read data from NVM region
+*
+* @note		Input address argument of write function in NVM region is
+* 			offset by defined start address of region!
+*
+* @param[in]	region	- NVM region defined in config table
+* @param[in]	addr	- Start region address + address
+* @param[in]	size	- Size of written data in bytes
+* @param[in]	p_data	- Pointer to read data
+* @return 		status	- Status of operation
+*/
+////////////////////////////////////////////////////////////////////////////////
 nvm_status_t nvm_read(const nvm_region_name_t region, const uint32_t addr, const uint32_t size, uint8_t * const p_data)
 {
 	nvm_status_t status = eNVM_OK;
@@ -102,13 +149,31 @@ nvm_status_t nvm_read(const nvm_region_name_t region, const uint32_t addr, const
 
 	// Check inputs
 	NVM_ASSERT( region < eNVM_REGION_NUM_OF );
-	NVM_ASSERT(( addr >= gp_nvm_regions[region].start_addr ) && (( addr + size ) < ( gp_nvm_regions[region].start_addr + gp_nvm_regions[region].size )));
+	NVM_ASSERT(		(( addr + gp_nvm_regions[region].start_addr ) < ( gp_nvm_regions[region].start_addr + gp_nvm_regions[region].size ))
+				&& 	(( addr + gp_nvm_regions[region].start_addr + size ) < ( addr + gp_nvm_regions[region].start_addr + gp_nvm_regions[region].size )));
 
-	gp_nvm_regions[region].p_driver->pf_nvm_read( addr, size, p_data );
+	// Read
+	if ( eNVM_OK != gp_nvm_regions[region].p_driver->pf_nvm_read( gp_nvm_regions[region].start_addr + addr, size, p_data ))
+	{
+		status = eNVM_ERROR;
+	}
 
 	return status;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/**
+*		Erase data from NVM region
+*
+* @note		Input address argument of write function in NVM region is
+* 			offset by defined start address of region!
+*
+* @param[in]	region	- NVM region defined in config table
+* @param[in]	addr	- Start region address + address
+* @param[in]	size	- Size of written data in bytes
+* @return 		status	- Status of operation
+*/
+////////////////////////////////////////////////////////////////////////////////
 nvm_status_t nvm_erase(const nvm_region_name_t region, const uint32_t addr, const uint32_t size)
 {
 	nvm_status_t status = eNVM_OK;
@@ -118,13 +183,17 @@ nvm_status_t nvm_erase(const nvm_region_name_t region, const uint32_t addr, cons
 
 	// Check inputs
 	NVM_ASSERT( region < eNVM_REGION_NUM_OF );
-	NVM_ASSERT(( addr >= gp_nvm_regions[region].start_addr ) && (( addr + size ) < ( gp_nvm_regions[region].start_addr + gp_nvm_regions[region].size )));
+	NVM_ASSERT(		(( addr + gp_nvm_regions[region].start_addr ) < ( gp_nvm_regions[region].start_addr + gp_nvm_regions[region].size ))
+				&& 	(( addr + gp_nvm_regions[region].start_addr + size ) < ( addr + gp_nvm_regions[region].start_addr + gp_nvm_regions[region].size )));
 
-	gp_nvm_regions[region].p_driver->pf_nvm_erase( addr, size );
+	// Erase
+	if ( eNVM_OK != gp_nvm_regions[region].p_driver->pf_nvm_erase( gp_nvm_regions[region].start_addr + addr, size ))
+	{
+		status = eNVM_ERROR;
+	}
 
 	return status;
 }
-
 
 ////////////////////////////////////////////////////////////////////////////////
 /**
